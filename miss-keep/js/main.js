2696143585e6f43a9597f48ebe7bb0f22a846fe7
ShiranCohen86 <shiranc86@gmail.com>
2021-02-24 10:16:44 +0200

    basic files creation

---
+++ miss-keep/js/main.js
@@ -0,0 +1,20 @@
+import { myRouter } from './routes.js'
+
+const options = {
+    el: '#app',
+    router: myRouter,
+    template: `
+        <section class="main-container-app">
+            <user-msg />
+            <app-header />
+            <router-view />
+        </section>
+    `,
+    components: {
+        appHeader,
+        userMsg,
+    
+    }
+}
+
+const app = new Vue(options)
