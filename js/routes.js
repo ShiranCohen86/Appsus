f4b9f4aa6589b06137a3bc0c52db2bb730d83f74
omeros <omer.golan.dev@gmail.com>
2021-02-24 19:12:52 +0200

    update4

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import homePage from './pages/home-page.cmp.js'
 // import about from './pages/about.cmp.js'
 import misterMail from './app/mister-mail/js/pages/mail-app.cmp.js'
-import missKeep from './app/miss-keep/js/pages/keep-app.js'
+import missKeep from './app/miss-keep/js/pages/keep-app.cmp.js'
 // import missbook from './app/mister-mail/js/pages/mail-app.js'
 
 const routes = [
