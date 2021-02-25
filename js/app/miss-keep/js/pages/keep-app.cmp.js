0180c6077522433ef9bb65a08413aac1b0a702e2
omeros <omer.golan.dev@gmail.com>
2021-02-25 16:18:20 +0200

    update11

--- js/app/miss-keep/js/pages/keep-app.cmp.js
+++ js/app/miss-keep/js/pages/keep-app.cmp.js
@@ -7,11 +7,11 @@ export default {
         <section class="keep-app flex justify-content-center align-items-center ">
             <div  class="grid-container " >
              
-                <textarea class="txt-area round" v-model="note1" style="overflow:hidden"  placeholder=""></textarea>
+                <textarea class="txt-area round" v-model="keep1" style="overflow:hidden"  placeholder=""></textarea>
               
-                <textarea class="txt-area round" v-model="note2" style="overflow:hidden"  placeholder=""></textarea>
+                <textarea class="txt-area round" v-model="keep2" style="overflow:hidden"  placeholder=""></textarea>
                
-                <textarea class="txt-area round" v-model="note3" style="overflow:hidden"  placeholder=""></textarea>
+                <textarea class="txt-area round" v-model="keep3" style="overflow:hidden"  placeholder=""></textarea>
             </div>
                 <button class="myButoon round"  @click.prevent="addKeep()" > add</button>
                 <button class="myButoon round"  @click.prevent="saveKeeps()" > Save</button>
@@ -24,37 +24,52 @@ export default {
 
         data() {
             return {
-                keeps = [],
-                note1 : null,
-                note2 : null,
-                note3 : null,
+                keep1 : null,
+                keep2 : null,
+                keep3 : null,
+                keepsArr  : [],
             
         
             }
         },
+        created() {
+            this.keepsArr[0]=this.keep1;
+            this.keepsArr[1]=this.keep3;
+            this.keepsArr[2]=this.keep3;
+            console.log('On Load Main !!');
+    
+        },
         methods: {
             saveKeeps(){
 
-               let  notes ={
-                    keep1 : this.note1,
-                    keep2 : this.note2,
-                    keep3 : this.note3,
-                }
-                console.log('saveKeeps',notes)
-                keepsService.saveAllKeeps(notes);
+            //    let  notes ={
+            //         keep1 : this.note1,
+            //         keep2 : this.note2,
+            //         keep3 : this.note3,
+            //     }
+            //     keeps.push(note1)
+            // this.keepsArr.forEach(element => {
+            //     keepsArr
+            // });
+            this.keepsArr[0]=this.keep1;
+            this.keepsArr[1]=this.keep2;
+            this.keepsArr[2]=this.keep3;
+
+                console.log('save button clicked to save Keeps',this.keepsArr)
+                keepsService.saveAllKeeps(this.keepsArr);
                 
             },
             loadKeeps(){
                 var keepsArr =keepsService.getKeepList()
-               .then(keepsArr=>{
-                console.log('Keeps from service in keep app after promis',keepsArr)
-                this.note1=keepsArr[0].keep1;
-                this.note2=keepsArr[0].keep2;
-                this.note3=keepsArr[0].keep3;
+               .then(DBkeepsArr=>{
+                console.log('Keeps from service in keep app after promis',DBkeepsArr)
+                this.keep1=DBkeepsArr[0][0];
+                this.keep2=DBkeepsArr[0][1];
+                this.keep3=DBkeepsArr[0][2];
                } )
          },
          addKeep(){
-
+             
          }
     },
     components: {
