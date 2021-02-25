efa481cd8249282fe7918c1ca00aec4ae7700f30
ShiranCohen86 <shiranc86@gmail.com>
2021-02-25 02:22:49 +0200

    more css

--- js/app/mister-mail/js/cmps/mail-preview.cmp.js
+++ js/app/mister-mail/js/cmps/mail-preview.cmp.js
@@ -3,8 +3,8 @@
 export default {
     props: ['mail'],
     template: `
-    <section class="mail-preview">
-            <img class="is-read-logo" :src="imgSrc" @click="toggleIsRead()"></p>
+    <section class="mail-preview" :class="{read : isRead}">
+            <img class="is-read-logo"  :src="imgSrc" @click="toggleIsRead()"></p>
             <h3>Mail subject:</h3>
             <p>{{mail.subject}}</p>
     </section>
@@ -18,12 +18,12 @@ export default {
     methods: {
         toggleIsRead() {
             this.isRead = !this.isRead
-            this.imgSrc = this.isRead ? '../css/apps/mister-mail/img/open-mail.png' : '../css/apps/mister-mail/img/close-mail.png';
+            this.imgSrc = this.isRead ? 'css/apps/mister-mail/img/open-mail.png' : 'css/apps/mister-mail/img/close-mail.png';
         },
     },
     created() {
         this.isRead = this.mail.isRead
-        this.imgSrc = this.isRead ? '../css/apps/mister-mail/img/open-mail.png' : '../css/apps/mister-mail/img/close-mail.png';
+        this.imgSrc = this.isRead ? 'css/apps/mister-mail/img/open-mail.png' : 'css/apps/mister-mail/img/close-mail.png';
     },
 
 }
