063ef8194dbd79d602e07ac64ec2b8d4d885ac8d
ShiranCohen86 <shiranc86@gmail.com>
2021-02-27 00:53:20 +0200

    buttons

--- js/app/mister-mail/js/cmps/mail-side-menu.cmp.js
+++ js/app/mister-mail/js/cmps/mail-side-menu.cmp.js
@@ -2,16 +2,19 @@
 export default {
     template: `
    <footer class="mail-side-menu">
-        <button @click="openCompose">Compose</button>
+        <button @click="openCompose" class="compose-btn">     
+          <img src="css/apps/mister-mail/img/plus-sign.png">
+          Compose
+        </button>
     </footer>`
     ,
-    data(){
-        return{
-            isCompose :true
+    data() {
+        return {
+            isCompose: true
         }
     },
     methods: {
-        openCompose(){
+        openCompose() {
             this.$emit('compose', this.isCompose)
         }
     },
