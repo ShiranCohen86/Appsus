2a084e9cb0dd5bf79431d98959a334674305fce4
ShiranCohen86 <shiranc86@gmail.com>
2021-02-28 01:04:40 +0200

    side menu button

--- js/app/mister-mail/js/cmps/mail-side-menu.cmp.js
+++ js/app/mister-mail/js/cmps/mail-side-menu.cmp.js
@@ -5,7 +5,22 @@ export default {
    <section class="mail-side-menu">
         <button @click="isCompose = !isCompose" class="compose-btn"> 
           <img src="css/apps/mister-mail/img/plus-sign.png">
-          Compose
+          <p>Compose</p>
+        </button>
+        <button @click="" class="inbox-btn"> 
+            <img src="css/apps/mister-mail/img/inbox.png">
+            <p>Inbox</p>
+            <p>{{isCompose}}</p>
+        </button>
+        <button @click="" class="star-btn"> 
+            <img class="star-logo" src="css/apps/mister-mail/img/star.png">
+            <p>Starred</p>
+            <p>{{isCompose}}</p>
+        </button>
+        <button @click="" class="sent-btn"> 
+            <img class="sent-logo" src="css/apps/mister-mail/img/sent.png">
+            <p>Sent</p>
+            <p>{{isCompose}}</p>
         </button>
         <mail-compose v-if="isCompose" />
     </section>`
