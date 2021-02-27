b794e849b7d4aa4166759be6c5bddfbf61ce34b0
omeros <omer.golan.dev@gmail.com>
2021-02-27 22:39:19 +0200

    update15

--- js/app/miss-keep/js/cmps/note-img.cmp.js
+++ js/app/miss-keep/js/cmps/note-img.cmp.js
@@ -1,23 +1,22 @@
 export default {
-    props: ['keepsArrDb'],
     template: `
   
      <div  class=" ">            
           
                 
                   <div contenteditable="true" >  
-                        <img  class="image"  src="https://www.coding-academy.org/books-photos/8.jpg"  width="200" height="200">
+                        <img  class="image round "  src="https://www.coding-academy.org/books-photos/8.jpg"  width="200" height="200">
                  </div>
                                          
                              <button class="delet-Butoon round"   >Save </button>
-                             <button class="delet-Butoon round" > Delet</button>
+                             <button class="delet-Butoon round"  @click.prevent="deleteKeep()"> Delet</button>
                  </div>
                 </div>
              
            </div>
    
     `,
-
+  props: ['info'],
     data() {
         return {
 
@@ -32,7 +31,10 @@ export default {
     methods: {
     
        
-       
+        deleteKeep(){
+            console.log('delete index in son note-img',this.info.index)
+            this.$emit("deletNote", this.info.index);
+        }
      
 
     },
